397635f0f91a34c4299df761a30ba2e469165cf6
Alvanerle <zzhumash03@gmail.com>
2022-05-20 01:11:32 +0600

    log from database finished

---
+++ src/main/java/ru/imangali/spring/models/User.java
@@ -0,0 +1,31 @@
+package ru.imangali.spring.models;
+
+public class User {
+    private String username;
+    private String password;
+    private boolean active;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
