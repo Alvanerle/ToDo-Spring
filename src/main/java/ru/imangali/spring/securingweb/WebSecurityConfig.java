3893cda50fe15a400a6ec7197e8257eb579dbb1e
Alvanerle <zzhumash03@gmail.com>
2022-05-20 00:32:59 +0600

    last updates

--- src/main/java/ru/imangali/spring/securingweb/WebSecurityConfig.java
+++ src/main/java/ru/imangali/spring/securingweb/WebSecurityConfig.java
@@ -17,15 +17,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
+                    .antMatchers("/").permitAll()
+                    .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
+                    .formLogin().defaultSuccessUrl("/tasks", true)
+                    .loginPage("/login")
+                    .permitAll()
                 .and()
-                .logout()
-                .permitAll();
+                    .logout()
+                    .permitAll();
     }
 
     @Bean
@@ -33,8 +33,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public UserDetailsService userDetailsService() {
         UserDetails user =
                 User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
+                        .username("u")
+                        .password("p")
                         .roles("USER")
                         .build();
 
